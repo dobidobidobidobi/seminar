25f8ebe23a162c3a6757411bda3d1e15cede934b
JoeBiden <dobiasj@gchd.cz>
2025-02-07 23:00:17 +0100

    MinesweeeperMVVM

---
+++ MinesweeperMVVM/MinesweeperMVVM/Model/Square.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinesweeperMVVM.Model
+{
+    internal struct Square
+    {
+        public bool IsMine { get; set; }
+        public bool IsFlagged { get; set; } = false;
+        public bool IsRevealed { get; set; } = false;
+        public int NeighbourMines { get; set; } = 0;
+
+        public Square() { }
+    }
+}
---
+++ MinesweeperMVVM/MinesweeperMVVM/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace MinesweeperMVVM.ViewModel
+{
+    internal class MainWindowViewModel : INotifyPropertyChanged
+    {
+        private string _selectedDifficulty;
+        public ObservableCollection<UIElement> Items { get; set; }
+        public ObservableCollection<string> Difficulties { get; set; }
+
+        public string SelectedDifficulty
+        {
+            get => _selectedDifficulty;
+            set
+            {
+                if (_selectedDifficulty != value)
+                {
+                    _selectedDifficulty = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        
+
+        public MainWindowViewModel()
+        {
+            Difficulties = new ObservableCollection<string> { "Lvl. 1 GOON", "Lvl. 100 Mafia Boss" };
+            _selectedDifficulty = Difficulties[0];
+            PropertyChanged = delegate { };
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
